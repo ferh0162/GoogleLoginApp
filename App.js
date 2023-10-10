73c9a89d1c53ca496a6c10909c55cabd8f4cf0b3
ferh0162 <ferh0162@stud.kea.dk>
2023-10-10 14:41:01 +0200

    Google login succefuld

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+// ios 1067276246859-vrqa0cjr2vjam36es534mad67o3ojr1a.apps.googleusercontent.com
+// android 1067276246859-2itpg3tig69jh3fbddaqr75j1po0o0cd.apps.googleusercontent.com
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const[request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:"1067276246859-2itpg3tig69jh3fbddaqr75j1po0o0cd.apps.googleusercontent.com"
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      console.log("Tilbage fra Google " + response.authentication.accessToken);
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vi arbejder på Google login</Text>
+      <Button
+      title="Google Login"
+      onPress={() => promptAsync()}
+      />
       <StatusBar style="auto" />
     </View>
   );
